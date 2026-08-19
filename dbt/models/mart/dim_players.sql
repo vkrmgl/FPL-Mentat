5f8360b3248bdfcd8ed1b31f09d486505d864093
Vikram Magal <vikram_magal@berkeley.edu>
2026-08-18 18:58:56 -0700

    Update

--- dbt/models/mart/dim_players.sql
+++ dbt/models/mart/dim_players.sql
@@ -7,6 +7,8 @@ select
 	corners_and_indirect_freekicks_order as corners_order,
 	direct_freekicks_order as fk_order,
 	penalties_order,
+	chance_of_playing_next_round,
+	chance_of_playing_this_round,
 	ownership_percent,
 	price as curr_price
 from {{ ref('stg_players') }}
--- dbt/models/staging/stg_players.sql
+++ dbt/models/staging/stg_players.sql
@@ -9,6 +9,8 @@ select
 	corners_and_indirect_freekicks_order,
 	direct_freekicks_order,
 	now_cost/10.0 as price,
+	cast(chance_of_playing_next_round as double) as chance_of_playing_next_round, 
+	cast(chance_of_playing_this_round as double) as chance_of_playing_this_round ,
 	selected_by_percent as ownership_percent,
 	ingested_at
 from {{ source('raw_layer','raw_players') }}
